129445be6166ee3dcfd8740bf55d80f9dce94618
Daniel Curtis <daniel17curtis@gmail.com>
2020-08-01 10:31:24 -0400

    Added YouTube shortcode for MDX

--- src/cms/cms-components.constants.js
+++ src/cms/cms-components.constants.js
@@ -1,12 +1,13 @@
-import React from "react"
+import React from "react";
 
-import { Heading, CallToAction, SmartLink } from "../components"
+import { Heading, CallToAction, SmartLink, YouTube } from "../components";
 
 // Include all components that will be parsed by MDX as React components here.
 // Any React component you'd like to allow your editors to use should be placed here.
 export const CMS_SHORTCODES = {
   CallToAction: props => <CallToAction {...props} />,
-}
+  YouTube: props => <YouTube {...props} />,
+};
 
 // Include any tags you'd like to replace with React components
 export const CMS_COMPONENTS = {
@@ -17,4 +18,4 @@ export const CMS_COMPONENTS = {
   h5: props => <Heading tag={5} {...props} />,
   h6: props => <Heading tag={6} {...props} />,
   a: props => <SmartLink {...props} />,
-}
+};
---
+++ src/components/YouTube.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const YouTube = ({ url }) => {
+  return (
+    <div className="video">
+      <iframe
+        src={url}
+        width="95%"
+        height="350px"
+        title="Title"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        frameBorder="0"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        allowFullScreen
+      />
+    </div>
+  );
+};
+
+YouTube.propTypes = {
+  url: PropTypes.string.isRequired,
+};
