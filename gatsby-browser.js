a51c2500b8f6457bccd2aceaaf3dbfb362314ce9
Daniel Curtis <daniel17curtis@gmail.com>
2020-08-06 23:42:58 -0400

    Added menu context

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,12 @@
+// Import local CSS
 import './src/app.css';
 import './src/components/components.css';
 import './src/templates/templates.css';
+
+// Import React context
+import React from 'react';
+import { MenuProvider } from './src/core/menu-context';
+
+export const wrapRootElement = ({ element }) => (
+	<MenuProvider>{element}</MenuProvider>
+);
--- src/components/menu.js
+++ src/components/menu.js
@@ -27,22 +27,17 @@ function Menu() {
 			query={query}
 			render={(data) => {
 				const menuArr = safelyGetSiteConfig(data.sitePage).menu_nav || [];
-				const style = {
-					textDecoration: 'none',
-					paddingLeft: '12px',
-					color: '#272727',
-				};
-				const activeStyle = {
-					color: '#0554f2',
-				};
 
 				return (
 					<div className={menu ? 'Menu' : 'Menu-hide'}>
-						<ul>
+						<ul className="Menu-ul">
 							{menuArr.map((item, i) => (
-								<li key={i}>
+								<li className="Menu-li" key={i}>
 									{item.url ? (
-										<Link to={item.url} style={style} activeStyle={activeStyle}>
+										<Link
+											activeClassName="Menu-link-active"
+											className="Menu-link"
+											to={item.url}>
 											{item.text}
 										</Link>
 									) : (
