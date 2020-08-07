1dc5fa47aad7d64f1cad060b7fbb49759fa52618
Daniel Curtis <daniel17curtis@gmail.com>
2020-08-07 19:16:16 -0400

    Updated quotations

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,15 @@
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby MDX Netlify Docs`,
-		description: `Docs starter for Gatsby-MDX + Netlify CMS`,
+		title: `MDX CMS Starter`,
+		description: `Documentation starter for Gatsby-MDX + Netlify CMS`,
 		author: `@codescurtis`,
 	},
 	plugins: [
 		`gatsby-plugin-dark-mode`,
 		`@pauliescanlon/gatsby-mdx-embed`,
 		`gatsby-plugin-react-helmet`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
@@ -15,14 +17,10 @@ module.exports = {
 				path: `${__dirname}/src/images`,
 			},
 		},
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-sharp`,
 		{
 			resolve: `gatsby-plugin-mdx`,
 			options: {
-				extensions: ['.mdx', '.md'],
-				gatsbyRemarkPlugins: [`gatsby-remark-autolink-headers`],
-				elements: [`h2`],
+				extensions: [`.mdx`, `.md`],
 				defaultLayouts: {
 					// This entry template will switch the page template based on
 					// a frontmatter value provided in the CMS, allowing users to
@@ -34,7 +32,7 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: `gatsby-starter-default`,
+				name: `gatsby-mdx-netlify`,
 				short_name: `starter`,
 				start_url: `/`,
 				background_color: `#663399`,
@@ -45,27 +43,25 @@ module.exports = {
 		},
 		{
 			resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it
-				trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+			options: {
+				// The property ID; the tracking code won`t be generated without it
+				trackingId: `YOUR_GOOGLE_ANALYTICS_TRACKING_ID`,
 				// Defines where to place the tracking script - `true` in the head and `false` in the body
 				head: false,
 				// Avoids sending pageview hits from custom paths
-				exclude: ["/admin/**"],
-		}
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.app/offline
-		// 'gatsby-plugin-offline',
+				exclude: [`/admin/**`],
+			},
+		},
 		{
-			resolve: 'gatsby-plugin-netlify-cms',
+			resolve: `gatsby-plugin-netlify-cms`,
 			options: {
 				modulePath: `${__dirname}/src/cms/cms.js`,
 				customizeWebpackConfig: (config) => {
 					config.node = {
 						...config.node,
-						fs: 'empty',
-						child_process: 'empty',
-						module: 'empty',
+						fs: `empty`,
+						child_process: `empty`,
+						module: `empty`,
 					};
 				},
 			},
